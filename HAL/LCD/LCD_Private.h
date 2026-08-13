000dde72d9555cd0a3d91805d8f4a030eacf5e00
ZIAD-ELMEKAWY <100296479+ZIAD-ELMEKAWY@users.noreply.github.com>
2026-08-13 19:01:32 +0300

    Create LCD_Private.h for LCD module
    Add header file for private LCD functions

---
+++ HAL/LCD/LCD_Private.h
@@ -0,0 +1,13 @@
+/*
+ *  file Name : LCD_Private.h
+ *  Created on: ??‏/??‏/???? at ?:??:?? ص
+ *  Author    : Ziad_Elmeakwy
+ *  Description : 
+ */
+
+#ifndef HAL_LCD_LCD_PRIVATE_H_
+#define HAL_LCD_LCD_PRIVATE_H_
+
+
+
+#endif /* HAL_LCD_LCD_PRIVATE_H_ */
