391e225b927589f87192e98bfef5451a96535608
ZIAD-ELMEKAWY <100296479+ZIAD-ELMEKAWY@users.noreply.github.com>
2026-08-15 10:34:29 +0300

    Create BUZ_Config.h for BUZ module configuration
    Initial configuration header for BUZ module.

---
+++ HAL/BUZ/BUZ_Config.h
@@ -0,0 +1,13 @@
+/*
+ *  file Name : BUZ_Config.h
+ *  Created on: ??‏/??‏/???? at ??:??:?? م
+ *  Author    : Ziad_Elmeakwy
+ *  Description : 
+ */
+
+#ifndef HAL_BUZ_BUZ_CONFIG_H_
+#define HAL_BUZ_BUZ_CONFIG_H_
+
+
+
+#endif /* HAL_BUZ_BUZ_CONFIG_H_ */
