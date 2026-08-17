43a02fbc970854665f650c9c8fedd18d0cb51578
ZIAD-ELMEKAWY <100296479+ZIAD-ELMEKAWY@users.noreply.github.com>
2026-08-17 19:42:00 +0300

    Create EXTI_Config.h

---
+++ MCAL/EXTI/EXTI_Config.h
@@ -0,0 +1,13 @@
+/*
+ *  file Name : EXTI_Config.h
+ *  Created on: ??‏/??‏/???? at ??:??:?? ص
+ *  Author    : Ziad_Elmeakwy
+ *  Description : 
+ */
+
+#ifndef MCAL_EXTI_EXTI_CONFIG_H_
+#define MCAL_EXTI_EXTI_CONFIG_H_
+
+
+
+#endif /* MCAL_EXTI_EXTI_CONFIG_H_ */
