84ba991f5adc6fc222a1c90db72c4c27cb512c05
ZIAD-ELMEKAWY <100296479+ZIAD-ELMEKAWY@users.noreply.github.com>
2026-08-17 19:14:43 +0300

    Add GIE_Interface header file with function declarations

---
+++ MCAL/GIE/GIE_Interface.h
@@ -0,0 +1,19 @@
+/*
+ *  file Name : GIE_Interface.h
+ *  Created on: ??‏/??‏/???? at ??:??:?? ص
+ *  Author    : Ziad_Elmeakwy
+ *  Description : 
+ */
+
+#ifndef MCAL_GIE_GIE_INTERFACE_H_
+#define MCAL_GIE_GIE_INTERFACE_H_
+
+#include "../../LIB/BIT_Math.h"
+#include "../../LIB/STD_Types.h"
+
+
+// Function Declaration
+void GIE_Enable(void);
+void GIE_Disable(void);
+
+#endif /* MCAL_GIE_GIE_INTERFACE_H_ */
