4d850376843f6f0da5b40287dcd2d6809aad65dd
ZIAD-ELMEKAWY <100296479+ZIAD-ELMEKAWY@users.noreply.github.com>
2026-08-17 19:15:05 +0300

    Create GIE_Private.h for GIE module
    Added header file for GIE with register definitions.

---
+++ MCAL/GIE/GIE_Private.h
@@ -0,0 +1,16 @@
+/*
+ *  file Name : GIE_Private.h
+ *  Created on: ??‏/??‏/???? at ??:??:?? ص
+ *  Author    : Ziad_Elmeakwy
+ *  Description : 
+ */
+
+#ifndef MCAL_GIE_GIE_PRIVATE_H_
+#define MCAL_GIE_GIE_PRIVATE_H_
+
+// Register addresses
+
+#define SREG	*((volatile u8*)0x5F)
+#define SREG_I	7
+
+#endif /* MCAL_GIE_GIE_PRIVATE_H_ */
