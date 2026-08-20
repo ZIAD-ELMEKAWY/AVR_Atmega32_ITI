5a61d772aa29ae095959fc89d92104f59e720a7a
ZIAD-ELMEKAWY <100296479+ZIAD-ELMEKAWY@users.noreply.github.com>
2026-08-20 19:09:40 +0300

    Implement DC motor control functions
    Added functions to initialize, turn on, and turn off a DC motor.

--- HAL/Motor/Motor_Program.c
+++ HAL/Motor/Motor_Program.c
@@ -8,23 +8,56 @@
 #include "Motor_Interface.h"
 
 
-void DCMOTOR_uint8Control (DCMOTOR_CONFIG * DcMotor , u8 Copy_uint8State)
+/* <<<<<<<<<<<<<<<<<<<<<<<<<<< User Interface Definition  >>>>>>>>>>>>>>>>>>>>>>>>>>>> */
+/***
+ * @name				DC_Motor_init
+ * @brief				Initializes a DC motor by setting its control pin as an output and ensuring it's initially off.
+ * @argument motor		A structure `motor_t` containing the port and pin information for the DC motor.
+ * @return				void
+ */
+void DC_Motor_init 	(motor_t motor)
 {
-
-	switch (Copy_uint8State)
+	if((motor.Port>= DIO_PORTA) && (motor.Port<= DIO_PORTD) )
 	{
-	case DCMOTOR_CW :
-		 DIO_enumSetPinValue(DcMotor->Copy_uint8DcMotorPort , DcMotor->Copy_uint8DcMotorPinA , DIO_PIN_LOW) ;
-		 DIO_enumSetPinValue(DcMotor->Copy_uint8DcMotorPort , DcMotor->Copy_uint8DcMotorPinB , DIO_PIN_HIGH) ;
-		 break ;
-	case DCMOTOR_CCW :
-		 DIO_enumSetPinValue(DcMotor->Copy_uint8DcMotorPort , DcMotor->Copy_uint8DcMotorPinB , DIO_PIN_LOW) ;
-		 DIO_enumSetPinValue(DcMotor->Copy_uint8DcMotorPort , DcMotor->Copy_uint8DcMotorPinA , DIO_PIN_HIGH) ;
-		 break ;
-	case DCMOTOR_STOP :
-		 DIO_enumSetPinValue(DcMotor->Copy_uint8DcMotorPort , DcMotor->Copy_uint8DcMotorPinA , DIO_PIN_LOW) ;
-		 DIO_enumSetPinValue(DcMotor->Copy_uint8DcMotorPort , DcMotor->Copy_uint8DcMotorPinB , DIO_PIN_LOW) ;
-		 break ;
+		if((motor.Pin >= DIO_PIN0) && (motor.Pin <= DIO_PIN7))
+		{
+			DIO_enumSetPinDirection(motor.Port, motor.Pin , DIO_PIN_OUTPUT);
+			DIO_enumSetPinValue(motor.Port , motor.Pin , DIO_PIN_LOW);
+		}
+	}
+}
+
+/***
+ * @name				DC_Motor_ON
+ * @brief				Turns on the DC motor by setting its control pin to a high value.
+ * @argument motor		A structure `motor_t` containing the port and pin information for the DC motor.
+ * @return				void
+ */
+void DC_Motor_ON 	(motor_t motor)
+{
+	if((motor.Port>= DIO_PORTA) && (motor.Port<= DIO_PORTD) )
+	{
+		if((motor.Pin >= DIO_PIN0) && (motor.Pin <= DIO_PIN7))
+		{
+			DIO_enumSetPinValue(motor.Port , motor.Pin , DIO_PIN_HIGH);
+		}
+	}
+}
+
+/***
+ * @name				DC_Motor_OFF
+ * @brief				Turns off the DC motor by setting its control pin to a low value.
+ * @argument motor		A structure `motor_t` containing the port and pin information for the DC motor.
+ * @return				void
+ */
+void DC_Motor_OFF (motor_t motor)
+{
+	if((motor.Port >= DIO_PORTA) && (motor.Port<= DIO_PORTD) )
+	{
+		if((motor.Pin >= DIO_PIN0) && (motor.Pin <= DIO_PIN7))
+		{
+			DIO_enumSetPinValue(motor.Port , motor.Pin , DIO_PIN_LOW);
+		}
 	}
 }
 
