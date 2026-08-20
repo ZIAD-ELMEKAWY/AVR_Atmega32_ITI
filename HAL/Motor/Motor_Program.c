22de2ce1ac35e51a20fd482afa6db65b4a9512ae
ZIAD-ELMEKAWY <100296479+ZIAD-ELMEKAWY@users.noreply.github.com>
2026-08-20 18:02:58 +0300

    Add DCMOTOR_uint8Control function
    Implement motor control function for DC motors.

--- HAL/Motor/Motor_Program.c
+++ HAL/Motor/Motor_Program.c
@@ -1,1 +1,30 @@
+/*
+ *  file Name : Motor_Program.c
+ *  Created on: ??‏/??‏/???? at ?:??:?? م
+ *  Author    : Ziad_Elmeakwy
+ *  Description : 
+ */
+
+#include "Motor_Interface.h"
+
+
+void DCMOTOR_uint8Control (DCMOTOR_CONFIG * DcMotor , u8 Copy_uint8State)
+{
+
+	switch (Copy_uint8State)
+	{
+	case DCMOTOR_CW :
+		 DIO_enumSetPinValue(DcMotor->Copy_uint8DcMotorPort , DcMotor->Copy_uint8DcMotorPinA , DIO_PIN_LOW) ;
+		 DIO_enumSetPinValue(DcMotor->Copy_uint8DcMotorPort , DcMotor->Copy_uint8DcMotorPinB , DIO_PIN_HIGH) ;
+		 break ;
+	case DCMOTOR_CCW :
+		 DIO_enumSetPinValue(DcMotor->Copy_uint8DcMotorPort , DcMotor->Copy_uint8DcMotorPinB , DIO_PIN_LOW) ;
+		 DIO_enumSetPinValue(DcMotor->Copy_uint8DcMotorPort , DcMotor->Copy_uint8DcMotorPinA , DIO_PIN_HIGH) ;
+		 break ;
+	case DCMOTOR_STOP :
+		 DIO_enumSetPinValue(DcMotor->Copy_uint8DcMotorPort , DcMotor->Copy_uint8DcMotorPinA , DIO_PIN_LOW) ;
+		 DIO_enumSetPinValue(DcMotor->Copy_uint8DcMotorPort , DcMotor->Copy_uint8DcMotorPinB , DIO_PIN_LOW) ;
+		 break ;
+	}
+}
 
